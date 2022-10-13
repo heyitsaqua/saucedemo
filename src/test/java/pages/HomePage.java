d9eb6097ed852a6cd8b3fcc2369e1cafa58d6c87
AianaSchool <dabanova_anna@mail.ru>
2022-10-13 19:43:37 -0400

    HomePage added

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+    @FindBy(id = "user-name")
+    public WebElement userInput;
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+    @FindBy(id = "login-button")
+    public WebElement loginButton;
+    @FindBy(className = "shopping_cart_link")
+    public WebElement ShoppingCartButton;
+    @FindBy(xpath = "//h3[@data-test='error']")
+    public WebElement errorText;
+}
+
