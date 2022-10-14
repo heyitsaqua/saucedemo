0d7796e22626573a4a3535ba28c7737150ca39a5
AianaSchool <dabanova_anna@mail.ru>
2022-10-13 20:23:19 -0400

    renamed LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,27 +3,27 @@ package tests;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTest extends BaseTest {
-    private HomePage homePage;
+    private LoginPage loginpage;
 
     @Test(testName = "US 301 - Verify standard_user can login with right password")
     public void test01() {
-        homePage.userInput.sendKeys("standard_user");
-        homePage.passwordInput.sendKeys("secret_sauce");
-        homePage.loginButton.click();
+        loginpage.userInput.sendKeys("standard_user");
+        loginpage.passwordInput.sendKeys("secret_sauce");
+        loginpage.loginButton.click();
 
-        Assert.assertTrue(homePage.ShoppingCartButton.isDisplayed());
+        Assert.assertTrue(loginpage.ShoppingCartButton.isDisplayed());
     }
 
     @Test(testName = "US 302 - Verify message with locked out user")
     public void test02() {
-        homePage.userInput.sendKeys("locked_out_user");
-        homePage.passwordInput.sendKeys("secret_sauce");
-        homePage.loginButton.click();
+       loginpage.userInput.sendKeys("locked_out_user");
+        loginpage.passwordInput.sendKeys("secret_sauce");
+        loginpage.loginButton.click();
         String expected = "Epic sadface: Sorry, this user has been locked out.";
-        String actual = homePage.errorText.getText();
+        String actual = loginpage.errorText.getText();
         Assert.assertEquals(actual, expected);
     }
 }
