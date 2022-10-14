a77136998eef495be202efab62b1021f632bcdb5
patcharapa hatsaenmuang <patcharapa,hat@gmail.com>
2022-10-13 21:19:24 -0400

    Hometest test

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,22 +2,33 @@ package tests;
 
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
+import java.util.List;
+
 public class HomeTest extends BaseTest {
     HomePage page;
-    @BeforeMethod
-    public void setUp(){
-       page = new HomePage(driver);
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        driver.findElement(By.id("user-name")).sendKeys("problem_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
     }
+
     @Test(testName = "US 303- When problem_user logs in all items on homepage should display same images")
-    public void test02(){
-        page.userInput.sendKeys("problem_user");
-        page.passwordInput.sendKeys("secret_sauce");
-        page.loginButton.click();
+    public void test01() {
+
+        List<WebElement> itemsPic = driver.findElements(By.
+                xpath("//img[@class='inventory_item_img']"));
+        itemsPic.forEach(buttons -> buttons.isDisplayed());
+
+
     }
 }
